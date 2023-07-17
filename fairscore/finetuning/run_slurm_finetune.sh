400d48b895c225ca6616b6fd02c34f9f4929cf5a
Eric Smith <EricMichaelSmith@users.noreply.github.com>
2023-07-17 10:25:20 -0400

    Add headers (#26)

--- fairscore/finetuning/run_slurm_finetune.sh
+++ fairscore/finetuning/run_slurm_finetune.sh
@@ -1,6 +1,12 @@
+#!/usr/bin/env python3
 
-#!/bin/bash
-## SLURM scripts have a specific format. 
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+## SLURM scripts have a specific format.
 
 ### Section1: SBATCH directives to specify job configuration
 
