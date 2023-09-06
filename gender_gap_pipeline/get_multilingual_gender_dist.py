be589f5a3ac6fd443a99238c4edd4e495b9a641f
Benjamin Muller <benjaminmuller@meta.com>
2023-09-06 11:23:05 -0700

    readme

--- gender_gap_pipeline/get_multilingual_gender_dist.py
+++ gender_gap_pipeline/get_multilingual_gender_dist.py
@@ -68,17 +68,25 @@ if __name__ == '__main__':
     if args.hf_datasets is not None:
         assert args.first_level_key is not None, '--args.first_level_key is required for hf_datasets: Provide one key per hf_datasets provided in --hg_datasets'
         if not args.lang_detect:
+            
+            if len(args.hf_datasets) < len(args.first_level_key):
+                assert len(args.hf_datasets) == 1, '--hf_datasets should be one dataset or a list of datasets as long as --first_level_key'
+                # if only one language provided, language assumed to be the same for all files and equal to --langs[0]
+                args.hf_datasets = [args.hf_datasets[0] for _ in args.first_level_key]
+            if len(args.split) < len(args.first_level_key):
+                assert len(args.split) == 1, '--split should be one split or a list of datasets as long as --first_level_key'
+                # if only one language provided, language assumed to be the same for all files and equal to --langs[0]
+                args.split = [args.split[0] for _ in args.first_level_key]
             if not len(args.hf_datasets) == len(args.langs):
-                # language assumed to be the same for all samples and equal to --langs[0]
+                # if only one language provided, language assumed to be the same for all files and equal to --langs[0]
                 args.langs = [args.langs[0] for _ in args.hf_datasets]
-        assert len(args.first_level_key) == len(args.hf_datasets) == len(args.first_level_key)
+        assert len(args.first_level_key) == len(args.hf_datasets) == len(args.split), 'The same number of --hf_datasets, --first_level_key and --split should be provided.'
         if args.second_level_key is not None:
-            assert len(args.first_level_key) == len(args.second_level_key)
+            assert len(args.first_level_key) == len(args.second_level_key), '--second_level_key is not null, it should include as many entries as --first_level_key'
         for i_dataset, dataset_name in enumerate(args.hf_datasets):
             hb_counter = GenderGAP(store_hb_dir='./tmp', lang=args.langs[i_dataset] if args.langs is not None else None, 
                                 ft_model_path='./fasttext_models/lid.176.bin' if args.lang_detect else None, 
                                 dataset_version=args.nouns_format_version)
-            #
             try:
                 dataset = load_dataset(dataset_name) 
                 hb_counter.process_dataset(dataset, split=args.split[i_dataset], 
@@ -94,7 +102,10 @@ if __name__ == '__main__':
                 else:
                     raise(e)
             stat = hb_counter.gender_dist(info_file=dataset_name)
-            reporting(report_df, hb_counter, args.langs[i_dataset] if args.langs is not None else 'Predicted', dataset_name)
+            label = dataset_name+'-'+args.split[i_dataset]+'-'+args.first_level_key[i_dataset]
+            if args.second_level_key is not None:
+                label += '-'+args.second_level_key[i_dataset]
+            reporting(report_df, hb_counter, args.langs[i_dataset] if args.langs is not None else 'Predicted', label)
             print(f'REPORT on {dataset_name}')
         
     # Processing text files or .txt.gz files
@@ -103,7 +114,7 @@ if __name__ == '__main__':
             args.file_dir = [args.file_dir[0] for _ in args.file_names]
         if not args.lang_detect:
             if not len(args.file_names) == len(args.langs):
-                # language assumed to be the same for all samples and equal to --langs[0]
+                # if only one language provided, language assumed to be the same for all files and equal to --langs[0]
                 args.langs = [args.langs[0] for _ in args.file_names]
         assert len(args.file_names) == len(args.file_dir), f'{len(args.file_names)} <> {len(args.file_dir)} '
         
