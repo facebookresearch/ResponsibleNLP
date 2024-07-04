cb31c6f21cdb8b736cde08c8c5d84da89e547ee6
Eric Smith <EricMichaelSmith@users.noreply.github.com>
2024-07-04 10:12:41 -0400

    Fix test_hf.py header (#47)
    Add in copyright

--- robbie/tests/metrics/test_hf.py
+++ robbie/tests/metrics/test_hf.py
@@ -1,3 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 import unittest
 
 from robbie.metrics import MetricConfig
